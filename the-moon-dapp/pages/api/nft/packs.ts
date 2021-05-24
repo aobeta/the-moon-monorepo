1145f908e6964f78dcb85fb82c0a6577ca81ec6a
Anthony Obeta <choobey21@gmail.com>
2021-05-23 20:52:44 -0700

    try specifying env variables before function declaration

--- the-moon-dapp/pages/api/nft/packs.ts
+++ the-moon-dapp/pages/api/nft/packs.ts
@@ -1,15 +1,12 @@
 import { getAllPacksForSale } from '@aobeta/flow-lib/scripts';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const moonPlatformAddress = process.env.MOON_PLATFORM_ACCOUNT_ADDRESS;
-const flowNode = process.env.FLOW_ACCESS_NODE;
+const moonPlatformAddress = process.env.MOON_PLATFORM_ACCOUNT_ADDRESS as string;
+const flowNode = process.env.FLOW_ACCESS_NODE as string;
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	try {
-		const address = process.env.MOON_PLATFORM_ACCOUNT_ADDRESS as string;
-		const flowAccessNode = process.env.FLOW_ACCESS_NODE as string;
-
-		const nftGroups = await getAllPacksForSale(address, flowAccessNode);
+		const nftGroups = await getAllPacksForSale(moonPlatformAddress, flowNode);
 
 		res.status(200).json(nftGroups);
 	} catch (error) {
