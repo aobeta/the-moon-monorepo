7896e58fce889122ef1d8862d2415d9bdfd7a582
Anthony Obeta <choobey21@gmail.com>
2021-05-23 20:38:13 -0700

    show error

--- the-moon-dapp/pages/api/nft/packs.ts
+++ the-moon-dapp/pages/api/nft/packs.ts
@@ -2,10 +2,14 @@ import { getAllPacksForSale } from '@aobeta/flow-lib/scripts';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	const address = process.env.MOON_PLATFORM_ACCOUNT_ADDRESS as string;
-	const flowAccessNode = process.env.FLOW_ACCESS_NODE as string;
+	try {
+		const address = process.env.MOON_PLATFORM_ACCOUNT_ADDRESS as string;
+		const flowAccessNode = process.env.FLOW_ACCESS_NODE as string;
 
-	const nftGroups = await getAllPacksForSale(address, flowAccessNode);
+		const nftGroups = await getAllPacksForSale(address, flowAccessNode);
 
-	res.status(200).json(nftGroups);
+		res.status(200).json(nftGroups);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
 };
